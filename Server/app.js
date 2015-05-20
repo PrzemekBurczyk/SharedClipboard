d054e1223ffe89e4769fbf4f46a0fb574778fe47
Jan Resiak <jan.resiak@sigmapoint.co>
2015-05-20 21:47:51 +0200

    server new events

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,16 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var clipboardSize = 5;
+var clipboard = {}
+
+for (var i = 1; i <= clipboardSize; i++) {
+	clipboard[i] = {
+				"type": "",
+				"value": ""
+			};
+}
+
 http.listen(3000, function () {
     console.log('listening on *:3000');
 }); 
@@ -16,6 +26,19 @@ io.on('connection', function (socket) {
     socket.on('hi', function (data) {
         console.log(data);
     });
+	
+	socket.on('text', function(data) {
+		console.log('received: ' + data);
+		clipboard[0].type = "text";
+		clipboard[0].value = data;
+	});
+	
+	socket.on('demand', function(data) {
+		if (data <= clipboardSize && data > 0) {
+			console.log('sending: ' + textData);
+			socket.emit('text', clipboard[data].value);
+		}
+	});
 
     socket.on('disconnect', function () {
         console.log('User disconnected');
