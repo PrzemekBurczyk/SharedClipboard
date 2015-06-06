23ab95bd7ee897f430205511acf2fd865fee9f70
Przemek Burczyk <przemek.burczyk@gmail.com>
2015-06-06 15:50:08 +0200

    Added procfile to integrate with Heroku.

--- Server/app.js
+++ Server/app.js
@@ -29,8 +29,10 @@ var requestedChanges = {}
 
 var CHANGE_TIMEOUT_MS = 10000;
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+var port = Number(process.env.PORT || 3000);
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
 
 var isNotEmpty = function (object) {
